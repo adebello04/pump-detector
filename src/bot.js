9853be997ed347c3b9c9ffc3d1d095db393ad177
slowworks <53411077+slowworks@users.noreply.github.com>
2021-05-23 02:40:05 +0100

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,8 @@ const bot = new Telegraf('1724870296:AAFVeLCTVWNUou6MCQkB2KZQ_qIG7IDzZKE')
 
 new PumpDetector(bot.telegram)
 
+bot.start((ctx) => ctx.reply('hello'))
+
 bot.catch(err => console.log('GLOBAL ERROR', err))
 
 module.exports = bot
